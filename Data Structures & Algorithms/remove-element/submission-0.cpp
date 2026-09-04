3758b3832f0a63ecd81fcce9e049663ee704f797
Hisham336 <90733131+Hisham336@users.noreply.github.com>
2026-09-04 10:10:57 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int count = 0; 
+
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == val){
+                nums[i] = INT_MAX;
+                count++;
+            }
+        }
+
+        int k = nums.size() - count;
+
+        sort(nums.begin(), nums.end());
+
+        return k;
+        
+    }
+};
