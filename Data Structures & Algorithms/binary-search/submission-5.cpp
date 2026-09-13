fa4e338c67496795216653163903cc101e4593ee
Hisham336 <90733131+Hisham336@users.noreply.github.com>
2026-09-13 17:23:34 +0530

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0, r = nums.size() - 1 ;
+
+        int m = l + ((r - l) / 2);
+
+        while (l <= r){
+
+            if(nums[m] == target){
+                return m;
+            }
+
+            if(nums[m] > target){
+                r = m-1;
+            }else {
+                l = m+1;
+            }
+            
+            m = l + ((r - l) / 2);
+        }
+
+        return -1;
+    }
+
+};
